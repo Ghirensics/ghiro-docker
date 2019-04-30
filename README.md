2cdc795fa4fb3e90e65579ac312f7261de0db5ab
Unknown <jonathanbatteas@gmail.com>
2019-04-30 16:31:14 -0400

    Updating all build files

---
+++ README.md
@@ -0,0 +1,15 @@
+To install Ghiro using Docker, from the root of this project run
+
+```
+docker-compose -f docker-compose.yml -f docker-compose-install.yml up -d
+```
+
+This will perform the migrations, and functions that must be run only once.
+
+To use Ghiro after the installation run:
+
+```
+docker-compose up -d
+```
+
+Ghiro will then be available in your browser at [http://localhost:9991/](http://localhost:9991/)
--- local_settings.py
+++ local_settings.py
@@ -7,10 +7,10 @@ DATABASES = {
         # Database name or path to database file if using sqlite3.
         'NAME': 'ghiro',
         # Credntials. The following settings are not used with sqlite3.
-        'USER': 'root',
+        'USER': 'ghiro',
         'PASSWORD': 'ghiromanager',
         # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
-        'HOST': '',
+        'HOST': 'pg',
         # Set to empty string for default port.
         'PORT': '',
         # Set timeout (avoids SQLite "database is locked" errors).
@@ -22,7 +22,7 @@ DATABASE_OPTIONS = {
  "init_command": "SET storage_engine=INNODB",
 }
 # Mongo database settings
-MONGO_URI = "mongodb://localhost/"
+MONGO_URI = "mongodb://mongo/"
 MONGO_DB = "ghirodb"
 # Max uploaded image size (in bytes).
 # Default is 150MB.
@@ -34,6 +34,7 @@ ALLOWED_EXT = ['image/bmp', 'image/x-canon-cr2', 'image/jpeg', 'image/png',
                'image/x-olympus-orf', 'image/x-photoshop', 'image/x-fuji-raf',
                'image/x-panasonic-raw2', 'image/x-tga', 'image/tiff', 'image/pjpeg',
                'image/x-x3f', 'image/x-portable-pixmap']
+AUTO_UPLOAD_DIR = "/var/www/ghiro/uploads"
 # Override default secret key stored in secret_key.py
 # Make this unique, and don't share it with anybody.
 # SECRET_KEY = "YOUR_RANDOM_KEY"
@@ -57,3 +58,13 @@ ALLOWED_HOSTS = ["*"]
 # Automatically checks once a day for updates.
 # Set it to False to disable update check.
 UPDATE_CHECK = True
+# these values were missing
+AUDITING_ENABLED = True
+JSON_EXPORT = True
+LOG_DIR = 'logs'
+LOG_PROCESSING_NAME = 'ghiro_log'
+LOG_PROCESSING_SIZE = 1e+8
+LOG_PROCESSING_NUM = 5
+LOG_AUDIT_NAME = 'ghiro_audit'
+LOG_AUDIT_SIZE = 1e+8
+LOG_AUDIT_NUM = 5
